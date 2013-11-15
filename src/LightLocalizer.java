74a0f1e58b3a505b30f2e8a960f43d45241b310f
Antonio D'Aversa <adaversa1@gmail.com>
2013-11-15 12:55:40 -0500

    please be right

--- src/LightLocalizer.java
+++ src/LightLocalizer.java
@@ -8,7 +8,6 @@
 
 import lejos.nxt.ColorSensor;
 import lejos.nxt.LCD;
-import lejos.nxt.LightSensor;
 import lejos.nxt.Sound;
 
 public class LightLocalizer {
@@ -16,7 +15,7 @@ public class LightLocalizer {
 	private TwoWheeledRobot robot;
 	private ColorSensor ls;
 	private Navigation nav;
-	//
+
 	private int firstLight;
 	private int count = 0; //counter
 	private double[] angles = {0, 0, 0, 0}; //initialize the angles to 0
@@ -30,23 +29,21 @@ public class LightLocalizer {
 		this.odo = odo;
 		this.robot = odo.getTwoWheeledRobot();
 		this.ls = ls;
-		this.nav = new Navigation(odo);
+		this.nav = new Navigation(this.odo);
 		// turn on the light
 		ls.setFloodlight(true);
 	}
 
 	public void doLocalization() {
-		// drive to location listed in tutorial
-		nav.travelTo(Math.sqrt(50), Math.sqrt(50));
-
 
 		// start rotating and clock all 4 gridlines
 		// we will rotate counter-clockwise so that we will measure the y first and third and
 		// x will be second and last.
 		
 		//set the turn speed higher to not detect the same line twice then rotate
-		this.robot.getLeftMotor().setSpeed(500);
-		this.robot.getRightMotor().setSpeed(500);
+	
+		
+		robot.setSpeeds(250, 250);
 		
 		robot.rotateCounterClockwise();
 		
@@ -55,7 +52,7 @@ public class LightLocalizer {
 			firstLight = this.ls.getNormalizedLightValue();
 			
 			//if the normalize light value is below a certain threshold we have detected a line
-			if(firstLight < 540){
+			if(firstLight < 460){
 				//beep whenever a line is detected.
 				Sound.beep();
 				//sleep here to not detect the same line twice (may be useless)
@@ -76,33 +73,48 @@ public class LightLocalizer {
 		}
 		
 		//reset speeds of the motors
-		this.robot.getLeftMotor().setSpeed(200);
-		this.robot.getRightMotor().setSpeed(200);
+		robot.getLeftMotor().setSpeed(200);
+		robot.getRightMotor().setSpeed(200);
 		
 		
 		// do trig to compute (0,0) and 0 degrees
 		
 		//Math taken from the tutorial slides.
+		//deltaTheta's are in degrees
 		deltaThetaX = angles[1] - angles[3];
 		deltaThetaY = angles[0] - angles[2];
+		
 		
 		x = -1 * d * Math.cos(Math.toRadians(deltaThetaY)/2);
 		y = -1 * d * Math.cos(Math.toRadians(deltaThetaX)/2);
 		
-		// when done travel to (0,0) and turn to 0 degrees
-		//set current position as temporary origin
-		odo.setPosition(new double[] { 0.0, 0.0, odo.getAng() },
-				new boolean[] { true, true, true });
+		double theta = 0;
+		
+		theta = 180 - angles[0] + deltaThetaY/2;
+		
+		LCD.drawInt((int)(deltaThetaY), 0, 4);
+		LCD.drawInt((int)(angles[0]), 0, 5);
+		
+		theta = -theta + odo.getAng();
+		
+		double [] pos = {x, y, theta};
+		boolean [] poop = {true, true, true};
+		
+		this.odo.setPosition(pos, poop );
+	
 		
 		//travel to the negative values of x and y to get to the actual origin
-		this.nav.travelTo(-x, -y);
+		//this.nav.travelTo(0, 0);
+		
 		//adjust the angle so that the the robot facing the y direction
-		nav.turnTo(Math.toRadians(0));
+		//nav.turnTo(Math.toRadians(0));
 		
 		//reset the position to (0,0,0)
-		LCD.clear();
-		odo.setPosition(new double[] { 0.0, 0.0, 0.0 },
-				new boolean[] { true, true, true });
+		
+		
 	}
+	
+	
+	
 
 }
